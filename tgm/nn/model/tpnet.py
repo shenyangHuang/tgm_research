1a7119cb5e094f03b0d2029897ecfb9793a18d8b
Andy Huang <shenyang.huang@mail.mcgill.ca>
2025-09-14 19:02:28 +0100

    setting right device

--- tgm/nn/model/tpnet.py
+++ tgm/nn/model/tpnet.py
@@ -431,6 +431,7 @@ class TPNet(nn.Module):
         src = src.repeat(2)
         dst = dst.repeat(2)
         edge_time = edge_time.repeat(2)
+        neighbours = neighbours.to(self.device)
         neighbor_node_features = X[neighbours, :]
         neighbor_node_features[neighbours == PADDED_NODE_ID] = 0
 
