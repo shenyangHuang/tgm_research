1d5da93b24e19171e288f86366b32799bd1ee522
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2025-09-30 15:47:46 -0400

    update epoch

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=long #unkillable #main #long
-#SBATCH --output=wiki_rewire_1.txt 
-#SBATCH --error=wiki_rewire_1_error.txt 
+#SBATCH --output=wiki_rewire_2.txt 
+#SBATCH --error=wiki_rewire_2_error.txt 
 #SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
 #SBATCH --gres=gpu:1                   # Ask for 1 titan xp gpu:rtx8000:1 
 #SBATCH --mem=32G #64G                             # Ask for 32 GB of RAM
@@ -11,4 +11,4 @@ module load python/3.10
 source $SCRATCH/my_venv/bin/activate
 pwd
 
-python -u examples/linkproppred/rewire_gcn.py --seed=1 --epochs=100 --device=cuda:0 --wandb
+python -u examples/linkproppred/rewire_gcn.py --seed=2 --epochs=500 --device=cuda:0 --wandb
