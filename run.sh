bf26802f5525408e49a3ac09931ed221065aba88
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2025-09-30 12:15:47 -0400

    updating gitignore and run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=unkillable #unkillable #main #long
+#SBATCH --partition=long #unkillable #main #long
 #SBATCH --output=wiki_rewire_1.txt 
 #SBATCH --error=wiki_rewire_1_error.txt 
 #SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
@@ -11,4 +11,4 @@ module load python/3.10
 source $SCRATCH/my_venv/bin/activate
 pwd
 
-python -u gcn_rewired.py --seed=1 --epochs=100 --device=cuda:0 --wandb
+python -u examples/linkproppred/rewire_gcn.py --seed=1 --epochs=100 --device=cuda:0 --wandb
