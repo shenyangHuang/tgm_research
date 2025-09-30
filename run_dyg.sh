5f234e942cb88105a915dd05e935a1943ebcbd10
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2025-09-30 11:20:56 -0400

    adding running scripts

--- run_dyg.sh
+++ run_dyg.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --partition=long #unkillable #main #long
-#SBATCH --output=tpnet_coin_3.txt 
-#SBATCH --error=tpnet_coin_3_error.txt 
+#SBATCH --output=dygformer_coin_3.txt 
+#SBATCH --error=dygformer_coin_3_error.txt 
 #SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
 #SBATCH --gres=gpu:a100l:1                   # Ask for 1 titan xp gpu:rtx8000:1 
-#SBATCH --mem=64G #64G                             # Ask for 32 GB of RAM
-#SBATCH --constraint="dgx&ampere"
+#SBATCH --mem=32G #64G                             # Ask for 32 GB of RAM
 #SBATCH --time=24:00:00    #48:00:00                   # The job will run for 1 day
 
 module load python/3.10
 source $SCRATCH/my_venv/bin/activate
 pwd
 
-python realtg_tpnet.py --seed=3 --epochs=30 --device=cuda:0 --dataset=tgbl-coin --bsize=10
+python realtg_dygformer.py --seed=3 --epochs=30 --device=cuda:0 --dataset=tgbl-coin
